636c3f10063982699cdc32faf6e2d1ff76ae9b8f
daniel-feup <dany_leal95@hotmail.com>
2019-05-26 23:32:49 +0200

    graph

--- receive/data.php
+++ receive/data.php
@@ -7,9 +7,12 @@
         $query = "INSERT INTO cell(voltage,current,anode_temp,cathode_temp,latitude, longitude)
                     VALUES(?,?,?,?,?,?)";
         
-        $latitude = 48.749210; 
-        $longitude =  9.101478;
-        $values = array($voltage, $current, $voltage, $voltage, $latitude, $longitude);
+        $latitude = 48.748587; 
+        $longitude =  9.101636;
+        $anode_temp = 80.01;
+        $cathode_temp = 79.8;
+
+        $values = array($voltage, $current, $anode_temp,$anode_temp,$latitude, $longitude);
 
         db();
         global $conn;
