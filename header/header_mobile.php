adffea829c2fa4956f078997db7b8e66fde5aefd
daniel-feup <dany_leal95@hotmail.com>
2019-05-27 21:04:19 +0200

    mobile

--- header/header_mobile.php
+++ header/header_mobile.php
@@ -5,38 +5,43 @@
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
                         <a class="logo" href="index.html">
-                            <img src="/dlr/images/dlr_logo_inv.png" width=50 height=50  /> <font color="white">&nbsp;Fuel Cell Cargo Pedelecs</font>
+                            <img src="/images/dlr_logo_inv.png" width=50 height=50  /> <font color="white">&nbsp;Fuel Cell Cargo Pedelecs</font>
                         </a>
 
                         <!-- com login -->
                         <?php if($_SESSION['user_id'] != NULL) { ?>
-                        <button class="hamburger hamburger--slider" type="button">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
-                        </button>
+                        
+                            <button class="hamburger hamburger--slider" type="button">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <nav class="navbar-mobile">
-                <div class="container-fluid">
-                    <ul class="navbar-mobile__list list-unstyled">
-                        <li class="has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-motorcycle"></i>Real Data</a>
-                        </li>
-                        <li>
-                            <a href="chart.html">
-                                <i class="fas fa-chart-bar"></i>Performance</a>
-                        </li>
-                        <li>
-                            <a href="/dlr/about/index.php">
-                                <i class="fas fa-table"></i>About</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+                <nav class="navbar-mobile">
+                    <div class="container-fluid">
+                        <ul class="navbar-mobile__list list-unstyled">
+                            <li class="has-sub">
+                                <a class="js-arrow" href="/realdata/index.php">
+                                    <i class="fas fa-motorcycle"></i>Real Data</a>
+                            </li>
+                            <li>
+                                <a href="/performance.php">
+                                    <i class="fas fa-chart-bar"></i>Performance</a>
+                            </li>
+                            <li>
+                                <a href="/about/index.php">
+                                    <i class="fas fa-table"></i>About</a>
+                            </li>
+                            <li>
+                                <a href="/faq.php">
+                                    <i class="fas fa-table"></i>FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
         
 
         <div class="sub-header-mobile-2 d-block d-lg-none">
@@ -44,7 +49,7 @@
                 <div class="account-wrap">
                     <div class="account-item account-item--style2 clearfix js-item-menu">
                         <div class="image">
-                            <img src="/dlr/images/icon/daniel.jpg" alt="John Doe" />
+                            <img src="/images/icon/daniel.jpg" alt="John Doe" />
                         </div>
                         <div class="content">
                             <a class="js-acc-btn" href="#"><?=$user_data['complete_name']?></a>
@@ -53,7 +58,7 @@
                             <div class="info clearfix">
                                 <div class="image">
                                     <a href="#">
-                                        <img src="/dlr/images/icon/daniel.jpg" alt="John Doe" />
+                                        <img src="/images/icon/daniel.jpg" alt="John Doe" />
                                     </a>
                                 </div>
                                 <div class="content">
@@ -75,7 +80,7 @@
                             
                             </div>
                             <div class="account-dropdown__footer">
-                                <a href="/dlr/logout.php">
+                                <a href="/logout.php">
                                     <i class="zmdi zmdi-power"></i>Logout</a>
                             </div>
                         </div>
