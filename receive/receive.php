3632146326db6256b9c55af86329fc5c4603f975
daniel-feup <dany_leal95@hotmail.com>
2019-05-30 23:34:10 +0200

    database

--- receive/receive.php
+++ receive/receive.php
@@ -2,15 +2,17 @@
 
 <?php
 
-    if(isset($_POST['voltage'])&& isset($_POST['current'])){
+    if(isset($_POST['voltage'])&& isset($_POST['current']) && isset($_POST['anode_temp'] && isset($_isset['cathode_temp'])){
 
         $query = "INSERT INTO cell(voltage,current,anode_temp,cathode_temp,latitude, longitude)
                     VALUES(?,?,?,?,?,?)";
         
         $latitude = 48.748587; 
         $longitude =  9.101636;
-        $anode_temp = 80.01;
-        $cathode_temp = 79.8;
+        //$anode_temp = 80.01;
+        //$cathode_temp = 79.8;
+        $anode_temp = $_POST['anode_temp'];
+        $cathode_temp = $POST['cathode_temp'];
         $voltage = $_POST['voltage'];
         $current = $_POST['current'];
 
